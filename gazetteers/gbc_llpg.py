8c0fb4b89f6c0e76c7a39d631b92208218e87b09
Ben <BforBen@users.noreply.github.com>
2015-09-28 15:33:51 +0100

    Add config for GBC LLPG webservice

---
+++ gazetteers/gbc_llpg.py
@@ -0,0 +1,23 @@
+﻿from json import loads
+from collections import namedtuple
+
+url = "https://www2.guildford.gov.uk/services/address/2/lookup/bysomething"
+params = {
+    'type': 'json',
+    'query': '##searchstring##',
+	'Scope': 'Local',
+	'IncludeHistorical': 'False',
+}
+
+
+def parseRequestResults(data, iface=None):
+    json_result = loads(data)
+    for item in json_result:
+        mapped = dict(item)
+        result = namedtuple('Result', ['description', 'x', 'y', 'zoom', 'epsg'])
+        result.description = mapped['FullAddress']
+        result.x = float(mapped['Easting'])
+        result.y = float(mapped['Northing'])
+        result.zoom = 1250
+        result.epsg = 27700
+        yield result
